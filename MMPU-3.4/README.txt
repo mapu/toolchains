97b78ff3bc9f9d6d7f14cd5b88c42708bf0efa0f
wangl <wangl@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2015-02-03 02:27:33 +0000

    wanglei: remove obsolete llvm 3.4 codes
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@30 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a

--- MMPU-3.4/README.txt
+++
@@ -1,9 +0,0 @@
-To-do's for MaPU MPU
------
-1. add BIUFALUSgl to support (D)
-
-
------------------------------------------------------------------
-future revision:
-1. SHUx.Ts = Tm IND Tn {(TB =+mm3)}{(C)|(!C)|()} is better to be written as:
-   Ts = SHUx.Tm IND Tn {(TB =+mm3)}{(C)|(!C)|()}
--- MaPUTest-3.4/MSPU/Linker/CrossLink.s
+++
@@ -1,9 +0,0 @@
-#RUN: echo -e ".global lab1\nNOP;  \nNOP;  \nLPTO lab1 @ (KI13)  ;\nNOP  ;\nlab1: \nNOP;" > %t.mmpu.s
-#RUN: llvm-mc -arch=mmpulite -filetype=obj -o %t.mmpu.o %t.mmpu.s
-#RUN: llvm-ld -T %s.m.ld -o %t.mmpu.elf %t.mmpu.o
-#RUN: llvm-mc -arch=mspu -filetype=obj -o %t.o %s
-#RUN: llvm-ld %t.o -o %t.elf --just-symbols %t.mmpu.elf
-#RUN: llvm-objdump -arch=mspu -d %t.elf | cut -b 128- | FileCheck %s
-.text 
-#CHECK: CallM 164;;
-CallM lab1;;
