2b112f74c81cd43a06fbd08e0d20bed4942f61d4
guoxy <guoxy@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2015-04-23 07:48:23 +0000

    the source of mainwindow
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@47 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a

--- MaPUSim/GUI/mainwindow.py
+++ MaPUSim/GUI/mainwindow.py
@@ -1,87 +1,87 @@
-#! /usr/bin/python
-# -*- coding: utf-8 -*-
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
+# -*- coding: utf-8 -*-   
+from PyQt4.QtGui import *  
+from PyQt4.QtCore import *  
+import sys  
+import qrc_resource  
 
-import res.qrc_resources
-
-QTextCodec.setCodecForTr(QTextCodec.codecForName("utf8"))
-
-class MainWindow(QMainWindow):
-    def __init__(self, parent = None):
-        super(MainWindow, self).__init__(parent)
-        self.setWindowTitle("MaPU Simulator")
-        self.createActions()
-        self.createMenus()
-        self.createToolBars()
-
-    def createActions(self):
-        self.fileOpenAction = QAction(QIcon(":/fileopen.png"), self.tr("打开"), self)
-        self.fileNewAction = QAction(QIcon(":/filenew.png"), self.tr("新建"), self)
-        self.fileSaveAction = QAction(QIcon(":/filesave.png"), self.tr("保存"), self)
-        self.exitAction = QAction(QIcon(":/filequit.png"), self.tr("退出"), self)
-        self.cutAction = QAction(QIcon(":/editcut.png"), self.tr("剪切"), self)
-        self.copyAction = QAction(QIcon(":/editcopy.png"), self.tr("复制"), self)
-        self.pasteAction = QAction(QIcon(":/editpaste.png"), self.tr("粘贴"), self)
-        self.aboutAction = QAction(self.tr("关于"), self)
-
-    def createMenus(self):
-        fileMenu=self.menuBar().addMenu(self.tr("文件"))
-        editMenu=self.menuBar().addMenu(self.tr("编辑"))
-        aboutMenu=self.menuBar().addMenu(self.tr("帮助"))
-
-    def createToolBars(self):
-        fileToolBar=self.addToolBar("File")
-        fileToolBar.addAction(self.fileNewAction)
-        fileToolBar.addAction(self.fileOpenAction)
-        fileToolBar.addAction(self.fileSaveAction)
-        editTool=self.addToolBar("Edit")
-        editTool.addAction(self.copyAction)
-        editTool.addAction(self.cutAction)
-        editTool.addAction(self.pasteAction)
-        self.setWindowTitle('MaPU Simulator')
-        self.statusBar().showMessage(self.tr("状态栏"))
+QTextCodec.setCodecForTr(QTextCodec.codecForName("utf8"))  
+  
+class MainWindow(QMainWindow):  
+    def __init__(self,parent=None):  
+        super(MainWindow,self).__init__(parent)         
+        self.setWindowTitle('MaPU Simulator')                        
+        self.createActions() 
+        self.createMenus()  
+        self.createToolBars()       
+     
+    def createActions(self): 
+        self.fileOpenAction=QAction(QIcon(":/fileopen.png"),self.tr("打开"),self)                                 
+        self.fileNewAction=QAction(QIcon(":/filenew.png"),self.tr("新建"),self)                  
+        self.fileSaveAction=QAction(QIcon(":/filesave.png"),self.tr("保存"),self)                               
+        self.exitAction=QAction(QIcon(":/filequit.png"),self.tr("退出"),self)              
+        self.cutAction=QAction(QIcon(":/editcut.png"),self.tr("剪切"),self)                  
+        self.copyAction=QAction(QIcon(":/editcopy.png"),self.tr("复制"),self)    
+        self.pasteAction=QAction(QIcon(":/editpaste.png"),self.tr("粘贴"),self)          
+        self.aboutAction=QAction(self.tr("关于"),self)   
+       
+    def createMenus(self):  
+        fileMenu=self.menuBar().addMenu(self.tr("文件"))                      
+        editMenu=self.menuBar().addMenu(self.tr("编辑"))          
+        aboutMenu=self.menuBar().addMenu(self.tr("帮助"))    
+        
+    def createToolBars(self):  
+        fileToolBar=self.addToolBar("File")    
+        fileToolBar.addAction(self.fileNewAction)  
+        fileToolBar.addAction(self.fileOpenAction)  
+        fileToolBar.addAction(self.fileSaveAction) 
+        editTool=self.addToolBar("Edit")  
+        editTool.addAction(self.copyAction)  
+        editTool.addAction(self.cutAction)  
+        editTool.addAction(self.pasteAction)                
+        self.statusBar().showMessage(self.tr("状态栏"))    
+        
         tab1=QDialog()
         tab2=QDialog()
-        tab3=QDialog()
+        self.tab3=QDialog()
         tab4=QDialog()
-        tabwidge=QTabWidget()
-        tabwidge.addTab(tab1,self.tr("模拟器信息"))
+        tabwidge=QTabWidget()         
+        tabwidge.addTab(tab1,self.tr("模拟器信息"))        
         tabwidge.addTab(tab2,self.tr("ARM视窗"))
-        tabwidge.addTab(tab3,self.tr("APC视窗"))
-        tabwidge.addTab(tab4,self.tr("配置与控制"))
+        tabwidge.addTab(self.tab3,self.tr("APC视窗"))
+        tabwidge.addTab(tab4,self.tr("配置与控制"))      
         self.setCentralWidget(tabwidge)
-        tab1.label1=QLabel("SoC")
+
+        tab1.label1=QLabel("SoC") #the detail of simulator
         tab1.label2=QLabel("ARM")
         tab1.label3=QLabel("APC")
-        tab1.button1=QPushButton(self.tr("刷新"))
-        tab1.button2=QPushButton(self.tr("刷新"))
-        tab1.button3=QPushButton(self.tr("刷新"))
+        tab1.button1=QPushButton(self.tr("刷新"))   
+        tab1.button2=QPushButton(self.tr("刷新")) 
+        tab1.button3=QPushButton(self.tr("刷新")) 
         self.text1=QTextEdit()
         self.text2=QTextEdit()
         self.text3=QTextEdit()
-        tab1.layout1=QHBoxLayout()
-        tab1.layout1.setMargin(15)
-        tab1.layout1.addWidget(tab1.label1)
-        tab1.layout1.addWidget(tab1.button1)
-        tab1.layout1.addWidget(tab1.label2)
-        tab1.layout1.addWidget(tab1.button2)
-        tab1.layout1.addWidget(tab1.label3)
-        tab1.layout1.addWidget(tab1.button3)
-        tab1.layout2=QHBoxLayout()
-        tab1.layout2.addWidget(self.text1)
-        tab1.layout2.addWidget(self.text2)
-        tab1.layout2.addWidget(self.text3)
-        tab1.mainLayout=QVBoxLayout()
-        tab1.mainLayout.setSpacing(10)
-        tab1.mainLayout.addLayout(tab1.layout1)
-        tab1.mainLayout.addLayout(tab1.layout2)
-        tab1.setLayout(tab1.mainLayout)
-        self.connect(tab1.button1,SIGNAL("clicked()"),self.slotClear1)
+
+        tab1.layout1=QHBoxLayout()  
+        tab1.layout1.setMargin(15)          
+        tab1.layout1.addWidget(tab1.label1) 
+        tab1.layout1.addWidget(tab1.button1)       
+        tab1.layout1.addWidget(tab1.label2)  
+        tab1.layout1.addWidget(tab1.button2)  
+        tab1.layout1.addWidget(tab1.label3)  
+        tab1.layout1.addWidget(tab1.button3)  
+        tab1.layout2=QHBoxLayout()  
+        tab1.layout2.addWidget(self.text1)  
+        tab1.layout2.addWidget(self.text2)  
+        tab1.layout2.addWidget(self.text3)           
+        tab1.mainLayout=QVBoxLayout()    
+        tab1.mainLayout.setSpacing(10)  
+        tab1.mainLayout.addLayout(tab1.layout1)  
+        tab1.mainLayout.addLayout(tab1.layout2) 
+        tab1.setLayout(tab1.mainLayout)        
+        self.connect(tab1.button1,SIGNAL("clicked()"),self.slotClear1)  
         self.connect(tab1.button2,SIGNAL("clicked()"),self.slotClear2)
         self.connect(tab1.button3,SIGNAL("clicked()"),self.slotClear3)
-        
-       self.tab3.Ltabapc=QDialog()  # the detail of APC
+        self.tab3.Ltabapc=QDialog()  # the detail of APC
         self.tab3.Lape0=QDialog()
         self.tab3.Lape1=QDialog()
         self.tab3.Lape2=QDialog()
@@ -105,7 +105,7 @@ class MainWindow(QMainWindow):
         self.tab3.connect(butt0,SIGNAL("clicked()"),self.slottab0)
         self.tab3.connect(butt1,SIGNAL("clicked()"),self.slottab1)
         self.tab3.connect(butt2,SIGNAL("clicked()"),self.slottab2)
-        self.tab3.connect(butt3,SIGNAL("clicked()"),self.slottab3)                                       
+        self.tab3.connect(butt3,SIGNAL("clicked()"),self.slottab3)                                      
         wDMA=QDialog()   
         nDMA=QDialog()
         station=QDialog()
@@ -148,7 +148,7 @@ class MainWindow(QMainWindow):
         self.tab3.mainLayout.addLayout(self.tab3.hlayout)
         self.tab3.mainLayout.addWidget(self.tab3.slider)
         self.tab3.setLayout(self.tab3.mainLayout)    
-         
+     
     def slotClear1(self):
         self.text1.clear()
 
@@ -169,18 +169,10 @@ class MainWindow(QMainWindow):
 
     def slottab3(self):
         self.tab3.Ltab.setCurrentWidget(self.tab3.Lape3)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                  
+app=QApplication(sys.argv) 
+main=MainWindow()  
+main.show()  
+app.exec_()         
+  
 
