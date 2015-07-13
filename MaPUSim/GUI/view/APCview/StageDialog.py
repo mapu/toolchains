24681df831efbc7d9d6d62c65f76873c50f67fce
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2015-07-13 06:13:22 +0000

    stage horizontal scroll move dynamic
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@152 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a

--- MaPUSim/GUI/view/APCview/StageDialog.py
+++ MaPUSim/GUI/view/APCview/StageDialog.py
@@ -24,25 +24,31 @@ class StageDialog(QDialog):
 	self.tableView.setRowCount(self.pageValue+1)
 	self.tableView.hideRow(self.pageValue)
 	self.tableView.horizontalHeader().setDefaultSectionSize(25)
-	stringList=QStringList()
-	stringList.clear()
-	for i in range(0,self.pageValue):
-	    stringList.append(str(i))
-	self.tableView.setVerticalHeaderLabels(stringList)
-	self.scroll=QScrollBar(Qt.Vertical)
+	self.verticalScroll=QScrollBar(Qt.Vertical)
 	self.maxValue=100
 	self.minValue=0
-	self.scroll.setRange(self.minValue,self.maxValue)
-	gridLay.addWidget(self.scroll)
-	self.connect(self.scroll,SIGNAL("valueChanged(int)"),self.currentValueSlot)
-	self.setLayout(gridLay)
-	#self.tableView.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 	self.curValue=0
+	self.verticalScroll.setRange(self.minValue,self.maxValue)
+	self.connect(self.verticalScroll,SIGNAL("valueChanged(int)"),self.currentVValueSlot)
+	self.horizontalScroll=QScrollBar(Qt.Horizontal)
+	self.maxTime=100
+	self.minTime=0
+	self.pageTime=100
+	self.curTime=0
+	self.tableView.setColumnCount(self.pageTime)
+	self.horizontalScroll.setRange(self.minTime,self.maxTime)
+	self.connect(self.horizontalScroll,SIGNAL("valueChanged(int)"),self.currentHValueSlot)
+	gridLay.addWidget(self.verticalScroll)
+	mainLay=QVBoxLayout()
+	mainLay.addLayout(gridLay)
+	mainLay.addWidget(self.horizontalScroll)
+	self.setLayout(mainLay)
+	self.tableView.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+	self.tableView.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
 	self.connect(self.tableView.horizontalHeader(),SIGNAL("sectionClicked(int)"),self.updateDialog)
 	self.slider=0
-	self.minTime=0
-	self.maxTime=100
-	self.tableView.setColumnCount(self.maxTime-self.minTime+1)
+	self.end=False
+	#self.oldList=[0]*self.pageValue
 	
     def updateDialog(self,column):
 	self.slider.setValue(column)
@@ -63,32 +69,77 @@ class StageDialog(QDialog):
 	    self.maxValue=len(self.snAll)
 	else:
 	    self.maxValue=0
-	self.scroll.setRange(self.minValue,self.maxValue)
+	    self.maxTime=0
+	self.row1=self.minValue
+	self.row2=self.pageValue+1
+	self.col1=self.minTime
+	self.col2=self.pageTime+1
+	self.verticalScroll.setRange(self.minValue,self.maxValue)
+	self.horizontalScroll.setRange(self.minTime,self.maxTime)
+
+    def currentHValueSlot(self,time):
+	if self.maxTime!=0 and self.maxValue!=0:
+	    if time<=self.pageTime/2:
+	        self.col1=self.minTime
+	        self.col2=self.pageTime+1
+		self.end=False
+		#self.oldList=[]
+		#for i in range(0,self.pageValue):
+		    #item=self.tableView.item(i,time-self.curTime)
+		    
+	    elif time>=self.maxTime-self.pageTime/2:
+	        self.col1=self.maxTime-self.pageTime-1
+	        self.col2=self.maxTime
+		#if self.end==False:
+		    #self.oldList=[]
+		#self.end=True	
+	    else:
+	        self.col1=time-self.pageTime/2
+	        self.col2=time+self.pageTime/2+1
+		self.end=False
+	        #self.oldList=[]
+
+	    self.curTime=time
+	    self.tableView.clear()
+	    self.tableView.setRowCount(self.pageValue+1)
+	    self.tableView.hideRow(self.pageValue)
+	    self.tableView.setColumnCount(self.pageTime)
+	    self.updateAPEDialog(self.row1,self.row2,self.col1,self.col2,self.end)
+	else:
+	    self.tableView.clear()
+	    self.tableView.setRowCount(self.pageValue+1)
+	    self.tableView.hideRow(self.pageValue)
+	    self.tableView.setColumnCount(self.pageTime)
 		
-    def currentValueSlot(self,value):
+    def currentVValueSlot(self,value):
 	self.curValue=value
-	self.tableView.clear()
-	self.tableView.setRowCount(self.pageValue+1)
-	self.tableView.hideRow(self.pageValue)
-	self.tableView.setColumnCount(self.maxTime-self.minTime+1)
+	if self.maxValue!=0 and self.maxTime!=0:
+	    if value<=self.pageValue/2:
+	        self.row1=self.minValue
+	        self.row2=self.pageValue+1
+	    elif value>=self.maxValue-self.pageValue/2:
+	        self.row1=self.maxValue-self.pageValue-1
+	        self.row2=self.maxValue
+	    else:
+	        self.row1=value-self.pageValue/2
+	        self.row2=value+self.pageValue/2+1
+	    self.tableView.clear()
+	    self.tableView.setRowCount(self.pageValue+1)
+	    self.tableView.hideRow(self.pageValue)
+	    self.tableView.setColumnCount(self.pageTime)
+	    self.updateAPEDialog(self.row1,self.row2,self.col1,self.col2)
+	else:
+	    self.tableView.clear()
+	    self.tableView.setRowCount(self.pageValue+1)
+	    self.tableView.hideRow(self.pageValue)
+	    self.tableView.setColumnCount(self.pageTime)
+
+    def updateAPEDialog(self,r1,r2,c1,c2,orientation=True):
 	stringList=QStringList()
 	stringList.clear()
-	if self.maxValue!=0:
-	    for i in range(self.minTime,self.maxTime+1):
-	        stringList.append(str(i))
+	for i in range(c1,c2):
+	    stringList.append(str(i))
 	    self.tableView.setHorizontalHeaderLabels(stringList)
-	    if value<=self.pageValue/2:
-	        r1=self.minValue
-	        r2=self.pageValue+1
-	    elif value>=self.maxValue-self.pageValue/2:
-	        r1=self.maxValue-self.pageValue-1
-	        r2=self.maxValue
-	    else:
-	        r1=value-self.pageValue/2
-	        r2=value+self.pageValue/2+1
-	    self.updateAPEDialog(r1,r2)
-
-    def updateAPEDialog(self,r1,r2):
 	for k in range(r1,r2):
 	    j=k-r1
 	    stringList=self.snAll[k]
@@ -96,36 +147,39 @@ class StageDialog(QDialog):
 	    self.tableView.setVerticalHeaderItem(j,QTableWidgetItem(str(stringList[4])+":"+stringList[7]))
 	    for i in range(0,20):
 		if stringList[9+i]!=-1:
-		    column=stringList[9+i]-self.minTime
-		    self.tableView.setItem(j,column,QTableWidgetItem(str(i-5)))	
-		    if i<5:
-			self.tableView.item(j,column).setBackground(QBrush(QColor("gray")))
-		    else:
-			self.tableView.item(j,column).setBackground(QBrush(QColor(193,210,255)))
-		    if temp!=-1:
-			item=self.tableView.item(j,temp)
-			for i in range(temp+1,column):
-			    self.tableView.setItem(j,i,QTableWidgetItem(item.text()))
-			    self.tableView.item(j,i).setBackground(item.background())					
-		    temp=column	
+		    if stringList[9+i]>=c1 and stringList[9+i]<c2:
+		        column=stringList[9+i]-c1
+		        self.tableView.setItem(j,column,QTableWidgetItem(str(i-5)))	
+		        if i<5:
+			    self.tableView.item(j,column).setBackground(QBrush(QColor("gray")))
+		        else:
+			    self.tableView.item(j,column).setBackground(QBrush(QColor(193,210,255)))
+		        if temp!=-1:
+			    item=self.tableView.item(j,temp)
+			    for i in range(temp+1,column):
+			        self.tableView.setItem(j,i,QTableWidgetItem(item.text()))
+			        self.tableView.item(j,i).setBackground(item.background())					
+		        temp=column	
 	    fetchall_sql = "SELECT * FROM "+self.dataBase.regTableName+" WHERE spumpu = "+"'"+self.flag+"'"+" and sn = "+str(stringList[4])
 	    r=self.dataBase.fetchall(self.APEdbFilePath,fetchall_sql)
 	    if r!=0:
 	    	for e in range(len(r)):
 		    stringList=r[e]
 		    if stringList[6]!="Misc Reg":
-		    	item=self.tableView.item(j,int(stringList[1])-self.minTime)
-		    	if item!=None:
-		    	    if stringList[5]=="W":
-		        	if item.background()==QBrush(QColor(0,255,0)):
-			    	    item.setBackground(QBrush(QColor(255,153,18)))
-		        	elif item.background()!=QBrush(QColor(255,153,18)):
-			    	    item.setBackground(QBrush(QColor(255,0,0)))
-		    	    elif stringList[5]=="R":
-		        	if item.background()==QBrush(QColor(255,0,0)):
-			   	    item.setBackground(QBrush(QColor(255,153,18)))
-		        	elif item.background()!=QBrush(QColor(255,153,18)):
-			    	    item.setBackground(QBrush(QColor(0,255,0)))
+			column=int(stringList[1])-c1
+			if column>=c1 and column<c2:
+		    	    item=self.tableView.item(j,column)
+		    	    if item!=None:
+		    	        if stringList[5]=="W":
+		        	    if item.background()==QBrush(QColor(0,255,0)):
+			    	        item.setBackground(QBrush(QColor(255,153,18)))
+		        	    elif item.background()!=QBrush(QColor(255,153,18)):
+			    	        item.setBackground(QBrush(QColor(255,0,0)))
+		    	        elif stringList[5]=="R":
+		        	    if item.background()==QBrush(QColor(255,0,0)):
+			   	        item.setBackground(QBrush(QColor(255,153,18)))
+		        	    elif item.background()!=QBrush(QColor(255,153,18)):
+			    	        item.setBackground(QBrush(QColor(0,255,0)))
 	    
     def wheelEvent(self,event):
      	if event.orientation()==Qt.Vertical:    
@@ -138,7 +192,7 @@ class StageDialog(QDialog):
     def resizeEvent(self,event):
 	self.pageValue=(self.height()-35)/self.rowHeight-1
 	self.tableView.setRowCount(self.pageValue)
-	self.currentValueSlot(self.curValue)
+	self.currentVValueSlot(self.curValue)
 
 
 
