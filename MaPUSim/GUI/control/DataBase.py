9109d17511b93f6ab13d5dad12619ba758bb7ab4
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2015-08-21 04:50:00 +0000

    stage
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@224 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a

--- MaPUSim/GUI/control/DataBase.py
+++ MaPUSim/GUI/control/DataBase.py
@@ -33,7 +33,7 @@ class DataBase():
     def createDatabase(self,num,path,progressCall):
 	self.progressCall=progressCall	
 	self.filePath=path
-	#self.filePath="/home/litt/trace.out"
+	self.filePath="/home/litt/trace.out"
         f=open(self.filePath,"r")
         lines=f.readlines()
 	self.maxValue=len(lines)
