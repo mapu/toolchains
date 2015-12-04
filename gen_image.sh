9b2dace0c6badd5af972a910059e2af75dfd2618
luoxq <luoxq@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2015-12-04 06:46:18 +0000

    update gen_image.sh, add mapu_defconfig
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@341 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a
    
    Former-commit-id: c20e6f1dc62cd345690c4b36124c8c040715060f

--- gen_image.sh
+++ gen_image.sh
@@ -6,21 +6,55 @@ root=`pwd`
 export PATH=$MAPU_HOME/arm-none-eabi/bin:$PATH
 export PATH=$MAPU_HOME/tools:$MAPU_HOME/arm/bin:$PATH
 
+# Build kernel
+cd $root
+if [ ! -e $root/kernel/arch/arm/boot/uImage -o ! -e $root/kernel/arch/arm/boot/mapu_sim.dtb ]
+then 
+  echo -e "\n\n\n\nMake kernel....\n"
+  if [ -e $root/kernel ]
+  then
+    cd $root/kernel
+    if [ -e $root/kernel/arch/arm/configs/mapu_defconfig ]
+    then
+      make distclean
+      cp $root/kernel/arch/arm/configs/mapu_defconfig .config
+      make uImage dtbs ARCH=arm CROSS_COMPILE=$MAPU_HOME/arm-linux-gnueabi/bin/arm-linux-gnueabi- -j64
+      if [ ! -e $root/kernel/arch/arm/boot/uImage -o ! -e $root/kernel/arch/arm/boot/dtb ]
+      then
+        echo -e "\n\n\n\nMake kernel fail: make uImage or dtb fail!\n"
+        exit
+      fi
+    else
+      echo -e "\n\n\n\nMake kernel fail: mapu_defconfig not exist!\n"
+      exit
+    fi
+  else 
+    echo -e "\n\n\n\nMake kernel fail: folder u-boot/ not exist!\n"
+    exit
+  fi
+fi
+
+
 # Build u-boot and ARM applications
+
+uboot_err=0
 
 cd $root
 if [ ! -e $root/u-boot/mapu/u-boot.bin ]
 then 
-  echo
-  echo
-  echo "Make u-boot.bin"
-  echo
+  echo -e "\n\n\n\nMake u-boot.bin....\n"
   if [ -e $root/u-boot ]
   then
     cd $root/u-boot
-    make O=mapu mapu_sim CONFIG_APP=y -j64
+    rm mapu/ -rf
+    make O=mapu mapu_sim CONFIG_APP=y || uboot_err=1
+    if [ "$uboot_err" -eq 1 ]
+    then
+      echo -e "\n\n\n\nMake u-boot fail: make u-boot.bin fail!\n"
+      exit
+    fi
   else 
-    echo "Make u-boot fail: folder u-boot/ not exist!"
+    echo -e "\n\n\n\nMake u-boot fail: folder u-boot/ not exist!\n"
     exit
   fi
 fi
@@ -30,16 +64,18 @@ fi
 cd $root
 if [ ! -e $root/boot_rom/main.bin ]
 then
-  echo
-  echo
-  echo "Make boot_rom!\n\n"
-  echo
+  echo -e "\n\n\n\nMake boot_rom!\n"
   if [ -e $root/boot_rom ]
   then
     cd $root/boot_rom
     make
+    if [ ! -e $root/boot_rom/main.bin ]
+    then
+      echo -e "\n\n\n\nMake boot_rom fail: make main.bin fail!\n"
+      exit
+    fi
   else
-    echo "Make boot_rom fail: folder boot_rom/ not exist!"
+    echo -e "\n\n\n\nMake boot_rom fail: folder boot_rom/ not exist!\n"
     exit
   fi
 fi
@@ -48,33 +84,35 @@ fi
 cd $root
 if [ ! -e $root/boot_rom/main.bin ]
 then 
-  echo "Make image fail: main.bin not exist!"
+  echo -e "\n\n\n\nMake image fail: main.bin not exist!\n\n"
   exit
 fi
 
 if [ ! -e $root/u-boot/mapu/u-boot.bin ]
 then 
-  echo "Make image fail: u-boot.bin not exist!"
+  echo -e "\n\n\n\nMake image fail: u-boot.bin not exist!\n"
   exit
 fi
 
 if [ ! -e $root/kernel/arch/arm/boot/mapu_sim.dtb ]
 then 
-  echo "Make image fail: mapu_sim.dtb not exist!"
+  echo -e "\n\n\n\nMake image fail: mapu_sim.dtb not exist!\n"
   exit
 fi
 
 if [ ! -e $root/kernel/arch/arm/boot/uImage ]
 then 
-  echo "Make image fail: uImage not exist!"
+  echo -e "\n\n\n\nMake image fail: uImage not exist!\n"
   exit
 fi
 
 if [ ! -e $root/ubuntu/ubuntu.img ]
 then 
-  echo "Make image fail: ubuntu.img not exist!"
+  echo -e "\n\n\n\nMake image fail: ubuntu.img not exist!\n"
   exit
 fi
+
+output=$1
 
 dd if=boot_rom/main.bin of=${output:-sim.img}
 dd bs=1k seek=64 if=u-boot/mapu/u-boot.bin of=${output:-sim.img} oflag=append
@@ -82,9 +120,6 @@ dd bs=1k seek=512 if=kernel/arch/arm/boot/mapu_sim.dtb of=${output:-sim.img}  of
 dd bs=1k seek=1K if=kernel/arch/arm/boot/uImage of=${output:-sim.img}  oflag=append
 dd bs=1k seek=3K if=ubuntu/ubuntu.img of=${output:-sim.img}  oflag=append
 
-echo
-echo
-echo
-echo "Make image done!"
+echo -e "\n\n\n\n\tMake image done!\n\n"
 echo
 
