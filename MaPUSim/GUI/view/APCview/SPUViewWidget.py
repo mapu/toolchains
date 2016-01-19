eb6603eef18823b1be26ff94708193295ac72e53
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2016-01-19 08:18:02 +0000

    add memtable widget
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@461 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a
    
    Former-commit-id: 52fd7677939c5532c2ddc4a0afad39d93d4cb187

--- MaPUSim/GUI/view/APCview/SPUViewWidget.py
+++ MaPUSim/GUI/view/APCview/SPUViewWidget.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from PyQt4.QtCore import SIGNAL, pyqtSlot
+from PyQt4.QtCore import Qt, SIGNAL, pyqtSlot
 from PyQt4.QtGui import QWidget, QPushButton, QVBoxLayout, QTabWidget,\
-    QTableWidget, QHBoxLayout
+    QTableWidget, QHBoxLayout, QSplitter
 from view.APCview.RegfileWidget import SPURegfileWidget, SpecialRegfileWidget
 from view.APCview.StageWidget import StageWidget
+from view.APCview.MemTableWidget import SPUMemTableWidget
 
 #QTextCodec.setCodecForTr(QTextCodec.codecForName("utf8"))
 
@@ -20,6 +21,7 @@ class SPUViewWidget(QWidget):
         stage_table = control.simDB.getTable("APE%dSPUStageTable" % idx)
         reg_table = control.simDB.getTable("APE%dSPURegTable" % idx)
         time_table = control.simDB.getTable("APE%dSPUTimeTable" % idx)
+        mem_table = control.simDB.getTable("APE%dSPUMemTable" % idx)
         self.stageWidget = StageWidget(inst_table, stage_table, reg_table, self)
         self.stageWidget.updateTimePointSignal.connect(control.simDB.setTimePointSlot)
         control.simDB.traceAnalyzeDone.connect(self.stageWidget.updatePageListSlot)
@@ -30,19 +32,25 @@ class SPUViewWidget(QWidget):
         # define rightTab
         self.genRegWidget = SPURegfileWidget(reg_table, time_table)
         self.specRegWidget = SpecialRegfileWidget(reg_table, time_table)
+        self.memTableWidget = SPUMemTableWidget(mem_table, inst_table)
         control.simDB.timeChanged.connect(self.genRegWidget.timeChangedSlot)
-        control.simDB.timeChanged.connect(self.genRegWidget.timeChangedSlot)
+        control.simDB.timeChanged.connect(self.specRegWidget.timeChangedSlot)
+        control.simDB.traceAnalyzeDone.connect(self.memTableWidget.updateTableSlot)
         
         self.traceWidget = QTableWidget()
         self.rightTab.addTab(self.genRegWidget, self.tr("General purpose RF"))
         self.rightTab.addTab(self.specRegWidget, self.tr("Special RF"))
+        self.rightTab.addTab(self.memTableWidget, self.tr("Mem Info"))
         self.rightTab.addTab(self.traceWidget, self.tr("Trace"))
         self.traceWidget.horizontalHeader().setStretchLastSection(True)
 
-        mainLayout = QHBoxLayout()
-        mainLayout.addWidget(self.leftWidget)
-        mainLayout.addWidget(self.rightTab)
-        mainLayout.setStretchFactor(self.leftWidget, 5)
-        mainLayout.setStretchFactor(self.rightTab, 2)
-        self.setLayout(mainLayout)
+        self.splitter = QSplitter(Qt.Horizontal)
+        self.splitter.setChildrenCollapsible(False)
+        self.splitter.addWidget(self.leftWidget)
+        self.splitter.addWidget(self.rightTab)
+        self.splitter.setStretchFactor(0, 2)
+        self.splitter.setStretchFactor(1, 1)
 
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.splitter)
+        self.setLayout(self.vbox)
