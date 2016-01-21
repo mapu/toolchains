2cf826573f27821c1fabf4c0b1a5be47046220d6
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2016-01-21 06:33:19 +0000

    add MPU widget
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@480 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a
    
    Former-commit-id: ff35a6dbd57b414cb12ad7384af56a8c197b8da9

---
+++ MaPUSim/GUI/view/APCview/MPUAllWidget.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*_
+from PyQt4.QtGui import QWidget, QVBoxLayout, QSplitter
+from PyQt4.QtCore import Qt
+from view.APCview.MPUCoreWidget import MPUCoreWidget
+
+class MPUAllWidget(QWidget):
+    def __init__(self, inst_table, parent = None):
+        super(MPUAllWidget, self).__init__(parent)
+
+        self.leftWidget = MPUCoreWidget(inst_table)
+        self.rightWidget = QWidget()
+
+        self.splitter = QSplitter(Qt.Horizontal)
+        self.splitter.setChildrenCollapsible(False)
+        self.splitter.addWidget(self.leftWidget)
+        self.splitter.addWidget(self.rightWidget)
+        self.splitter.setStretchFactor(0, 12)
+        self.splitter.setStretchFactor(1, 1)
+
+        self.vbox = QVBoxLayout()
+        self.vbox.addWidget(self.splitter)
+        self.setLayout(self.vbox)
+
+
