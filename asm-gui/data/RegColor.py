485581c11eb0d28c047ed3282b092608db3c890c
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2016-02-04 07:03:44 +0000

    painter loop reg
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@514 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a
    
    Former-commit-id: 8accb1af0c08c1b766543d7ee13605a517c0400a

---
+++ asm-gui/data/RegColor.py
@@ -0,0 +1,12 @@
+from PyQt4.QtCore import Qt
+def initRegColor():
+    '''
+    Define color for reg
+    '''      
+    RegColors = {"0" : Qt.red,
+                 "1" : Qt.green,
+                 "2" : Qt.blue}
+
+    return RegColors
+
+
