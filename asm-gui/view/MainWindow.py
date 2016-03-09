b6d9ad963f72d82047cf713b0495e72848385093
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2016-03-09 07:22:49 +0000

    mainwindow
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@540 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a
    
    Former-commit-id: cb4567f5b2fdff639ce875ea297d1fef335d25b5

--- asm-gui/view/MainWindow.py
+++ asm-gui/view/MainWindow.py
@@ -1,27 +1,25 @@
 # -*- coding: utf-8 -*-   
-from PyQt4.QtGui import QMainWindow, QToolBar, QAction, QIcon, QCheckBox, QTextEdit, QFileDialog
+from PyQt4.QtGui import QMainWindow, QToolBar, QAction, QIcon, QCheckBox, QTextEdit, QFileDialog, QLineEdit
 from PyQt4.QtCore import pyqtSignal, Qt, SIGNAL, pyqtSlot
 from res import qrc_resources 
-from view.MicrocodeTable.MicrocodeTable import MicrocodeTable
+from view.MicrocodeTableWidget.MicrocodeTableWidget import MicrocodeTableWidget
 from Utils import warning
+
 
 class MainWindow(QMainWindow):  
     def __init__(self, parent = None):  
         super(MainWindow, self).__init__(parent)   
 
+        self.register = ["KI12", "KI13", "KI14", "KI15"]
         self.setWindowTitle("Graphic Microcode Editor")
         self.resize(800, 600)
-
-        self.microcodeTable = MicrocodeTable()
-
-        self.setCentralWidget(self.microcodeTable)
-
+        self.microcodeTableWidget = MicrocodeTableWidget(self.register)
+        self.setCentralWidget(self.microcodeTableWidget)
         self.createAction()
         self.createContextMenu()
         self.createToolBars()
         self.createMenus()
-
-        self.microcodeTable.itemRegStateSignal.connect(self.itemRegStateSlot)
+        self.microcodeTableWidget.itemRegStateSignal.connect(self.itemRegStateSlot)
 
     def createAction(self):
         self.newAction = QAction(self.tr('New'), self)
@@ -48,41 +46,41 @@ class MainWindow(QMainWindow):
         self.cutAction = QAction(QIcon(":/cut.png"), self.tr("Cut"), self)
         self.cutAction.setShortcut("Ctrl+X")
         self.cutAction.setStatusTip("Cut the current selection's contents to the clipboard")
-        self.connect(self.cutAction, SIGNAL("triggered()"), self.microcodeTable.cut)
+        self.connect(self.cutAction, SIGNAL("triggered()"), self.microcodeTableWidget.cut)
 
         self.copyAction = QAction(QIcon(":/copy.png"), self.tr("Copy"), self)
         self.copyAction.setShortcut("Ctrl+C")
         self.copyAction.setStatusTip("Copy the current selection's contents to the clipboard")
-        self.connect(self.copyAction, SIGNAL("triggered()"), self.microcodeTable.copy)
+        self.connect(self.copyAction, SIGNAL("triggered()"), self.microcodeTableWidget.copy)
 
         self.pasteAction = QAction(QIcon(":/paste.png"), self.tr("Paste"), self)
         self.pasteAction.setShortcut("Ctrl+V")
         self.pasteAction.setStatusTip("Paste the clipboard's contents into the current selection")
-        self.connect(self.pasteAction, SIGNAL("triggered()"), self.microcodeTable.paste)
+        self.connect(self.pasteAction, SIGNAL("triggered()"), self.microcodeTableWidget.paste)
 
-        self.deleteAction = QAction(self.tr("Delete"), self)
-        self.deleteAction.setShortcut("Del")
-        self.deleteAction.setStatusTip("Delete the current selection's contents")
-        self.connect(self.deleteAction, SIGNAL("triggered()"), self.microcodeTable.delete)
+        self.clearAction = QAction(self.tr("Clear"), self)
+        self.clearAction.setShortcut("Del")
+        self.clearAction.setStatusTip("Delete the current selection's contents")
+        self.connect(self.clearAction, SIGNAL("triggered()"), self.microcodeTableWidget.delete)
 
         self.selectRowAction = QAction(self.tr("Row"), self)
         self.selectRowAction.setStatusTip("Select all the cells in the current row")
-        self.connect(self.selectRowAction, SIGNAL("triggered()"), self.microcodeTable.selectCurrentRow)
+        self.connect(self.selectRowAction, SIGNAL("triggered()"), self.microcodeTableWidget.selectCurrentRow)
 
         self.selectColumnAction = QAction(self.tr("Column"), self)
         self.selectColumnAction.setStatusTip("select all the cells in the current column")
-        self.connect(self.selectColumnAction, SIGNAL("triggered()"), self.microcodeTable.selectCurrentColumn)
+        self.connect(self.selectColumnAction, SIGNAL("triggered()"), self.microcodeTableWidget.selectCurrentColumn)
 
         self.selectAllAction = QAction(self.tr("All"), self)
         self.selectAllAction.setShortcut("Ctrl+A")
         self.selectAllAction.setStatusTip("Select all the cells in the Microcode table")
-        self.connect(self.selectAllAction, SIGNAL("triggered()"), self.microcodeTable.selectAll)
+        self.connect(self.selectAllAction, SIGNAL("triggered()"), self.microcodeTableWidget.selectAll)
 
         self.insertCellAction = QAction(self.tr("Insert"), self)
-        self.connect(self.insertCellAction, SIGNAL("triggered()"), self.microcodeTable.insertDialogShow)
+        self.connect(self.insertCellAction, SIGNAL("triggered()"), self.microcodeTableWidget.insertDialogShow)
 
         self.deleteCellAction = QAction(self.tr("Delete"),self)
-        self.connect(self.deleteCellAction, SIGNAL("triggered()"), self.microcodeTable.deleteDialogShow)
+        self.connect(self.deleteCellAction, SIGNAL("triggered()"), self.microcodeTableWidget.deleteDialogShow)
 
     def createMenus(self):
         fileMenu = self.menuBar().addMenu("File")
@@ -96,7 +94,7 @@ class MainWindow(QMainWindow):
         editMenu.addAction(self.cutAction)
         editMenu.addAction(self.copyAction)
         editMenu.addAction(self.pasteAction)
-        editMenu.addAction(self.deleteAction)
+        editMenu.addAction(self.clearAction)
 
         selectSubMenu = editMenu.addMenu("Select")
         selectSubMenu.addAction(self.selectRowAction)
@@ -104,12 +102,13 @@ class MainWindow(QMainWindow):
         selectSubMenu.addAction(self.selectAllAction)
 
     def createContextMenu(self):
-        self.microcodeTable.addAction(self.cutAction)
-        self.microcodeTable.addAction(self.copyAction)
-        self.microcodeTable.addAction(self.pasteAction)
-        self.microcodeTable.addAction(self.insertCellAction)
-        self.microcodeTable.addAction(self.deleteCellAction)
-        self.microcodeTable.setContextMenuPolicy(Qt.ActionsContextMenu)
+        self.microcodeTableWidget.addAction(self.cutAction)
+        self.microcodeTableWidget.addAction(self.copyAction)
+        self.microcodeTableWidget.addAction(self.pasteAction)
+        self.microcodeTableWidget.addAction(self.clearAction)
+        self.microcodeTableWidget.addAction(self.insertCellAction)
+        self.microcodeTableWidget.addAction(self.deleteCellAction)
+        self.microcodeTableWidget.setContextMenuPolicy(Qt.ActionsContextMenu)
 
     def createToolBars(self):
         fileToolBar = self.addToolBar("File")
@@ -122,70 +121,112 @@ class MainWindow(QMainWindow):
         editToolBar.addAction(self.copyAction)
         editToolBar.addAction(self.pasteAction)
 
-        registerToolBar=self.addToolBar("Register")
-        self.register0Check=QCheckBox("Reg0") 
-        self.register1Check=QCheckBox("Reg1") 
+        registerToolBar = self.addToolBar("Register")
+        self.register0Check = QCheckBox(self.register[0]) 
+        self.register0Text = QLineEdit("0")
+        self.register0Text.setFixedSize(40, 20)
+        #self.register0Text.setEnabled(False)
+        self.register1Check = QCheckBox(self.register[1]) 
+        self.register1Text = QLineEdit("0")
+        self.register1Text.setFixedSize(40, 20)
+        #self.register1Text.setEnabled(False)
+        self.register2Check = QCheckBox(self.register[2]) 
+        self.register2Text = QLineEdit("0")
+        self.register2Text.setFixedSize(40, 20)
+        #self.register2Text.setEnabled(False)
+        self.register3Check = QCheckBox(self.register[3])
+        self.register3Text =QLineEdit("0")
+        self.register3Text.setFixedSize(40, 20)
+        #self.register3Text.setEnabled(False)
         self.registerCheck = []
         self.registerCheck.append(self.register0Check)
         self.registerCheck.append(self.register1Check)
-        self.connect(self.register0Check, SIGNAL("clicked(bool)"), self.register0Slot)
-        self.connect(self.register1Check, SIGNAL("clicked(bool)"), self.register1Slot)
+        self.registerCheck.append(self.register2Check)
+        self.registerCheck.append(self.register3Check)
+        self.registerText = []
+        self.registerText.append(self.register0Text)
+        self.registerText.append(self.register1Text)
+        self.registerText.append(self.register2Text)
+        self.registerText.append(self.register3Text)
+        self.connect(self.register0Check, SIGNAL("clicked(bool)"), self.registerCheckSlot)
+        self.connect(self.register1Check, SIGNAL("clicked(bool)"), self.registerCheckSlot)
+        self.connect(self.register2Check, SIGNAL("clicked(bool)"), self.registerCheckSlot)
+        self.connect(self.register3Check, SIGNAL("clicked(bool)"), self.registerCheckSlot)
         registerToolBar.addWidget(self.register0Check)
+        registerToolBar.addWidget(self.register0Text)
         registerToolBar.addWidget(self.register1Check)
+        registerToolBar.addWidget(self.register1Text)
+        registerToolBar.addWidget(self.register2Check)
+        registerToolBar.addWidget(self.register2Text)
+        registerToolBar.addWidget(self.register3Check)
+        registerToolBar.addWidget(self.register3Text)
 
+    @pyqtSlot()
     def newFile(self):
-        self.microcodeTable.clearTable()
+        self.microcodeTableWidget.loopBodyList = [[]for i in xrange(self.microcodeTableWidget.ColumnCount)]
+        self.microcodeTableWidget.initTable()
 
+    @pyqtSlot()
     def openFile(self):
         fileName = QFileDialog.getOpenFileName(self, self.tr("select file"), "/")
-        self.microcodeTable.openFile(fileName)
+        if fileName != "":
+            self.microcodeTableWidget.initTable()
+            self.microcodeTableWidget.openFile(fileName)
 
+    @pyqtSlot()
     def saveFile(self):
         fileName = QFileDialog.getSaveFileName(self, self.tr("Save File"), "untitled.png")
-        self.microcodeTable.saveFile(fileName)
+        if fileName != "":
+            self.microcodeTableWidget.saveFile(fileName)
 
+    @pyqtSlot()
     def closeWindow(self):
-        print "closeWindow"
+        self.close()
 
     def closeEvent(self, event):
         self.closeWindow()
-	
-    def register0Slot(self, checkState):
-        color = Qt.red
-        if checkState == True:
-            re = self.microcodeTable.paintRect(0, color) 
-            if re == 0:
-                self.register0Check.setCheckState(Qt.Unchecked)
-                warning("Please select one column")
-            if re == -1:
-                self.register0Check.setCheckState(Qt.Unchecked)
-                warning("Please select one or more cells")       
-        else:
-            self.microcodeTable.eraserRect(0)
 
-    def register1Slot(self, checkState):
-        color = Qt.green
-        if checkState == True:
-            re = self.microcodeTable.paintRect(1, color)   
-            if re == 0:
-                self.register1Check.setCheckState(Qt.Unchecked)   
-                warning("Please select one column")  
-            if re == -1:
-                self.register1Check.setCheckState(Qt.Unchecked)
-                warning("Please select one or more cells")  
-        else:
-            self.microcodeTable.eraserRect(1) 
+    @pyqtSlot(bool)	
+    def registerCheckSlot(self, checkState):
+        if self.sender() in self.registerCheck:
+            idx = self.registerCheck.index(self.sender())
+            if checkState == True:
+                re = self.microcodeTableWidget.paintRect(idx, self.registerText[idx].text()) 
+                if re == 0:
+                    self.registerCheck[idx].setCheckState(Qt.Unchecked)
+                    warning("Please select one column")
+                    return
+                if re == -1:
+                    self.registerCheck[idx].setCheckState(Qt.Unchecked)
+                    warning("Please select one or more cells") 
+                    return
+                if re == -2:
+                    self.registerCheck[idx].setCheckState(Qt.Unchecked)
+                    warning("Cannot set loop at the intersection of cells")   
+                    return
+                self.registerText[idx].setEnabled(True)   
+            else:
+                self.microcodeTableWidget.eraserRect(idx)
+                #self.registerText[idx].setEnabled(False)
 
+    @pyqtSlot(bool)
     def setAllCheckStatus(self, status):
         num = len(self.registerCheck)
         for i in xrange(0, num):
             self.registerCheck[i].setCheckState(status)
+            self.registerText[i].clear()
+            #self.registerText[i].setEnabled(False)
 
-    def itemRegStateSlot(self, reg):
+    @pyqtSlot(list, list)
+    def itemRegStateSlot(self, regList, textList):
         self.setAllCheckStatus(Qt.Unchecked)
-        num = len(reg)
+        num = len(regList)
         for i in xrange(0, num):
-            self.registerCheck[reg[i]].setCheckState(Qt.Checked)
+            self.registerCheck[int(regList[i])].setCheckState(Qt.Checked)
+            self.registerText[int(regList[i])].setEnabled(True)
+        num = len(textList)
+        for i in xrange(num):
+            self.registerText[i].setText(str(textList[i]))
 
                 
 
