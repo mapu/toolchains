8b43745ec659cb4da480c4b671ab11e1f6e6878d
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2016-04-21 08:47:32 +0000

    lij:add test file
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@613 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a
    
    Former-commit-id: 1dc44d32762767722d39d7c6bbd4af7d6ac08762

---
+++ asm-gui/test/testdeleteCellLeftMul.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+#encoding: utf-8
+
+from PyQt4.QtGui import QWidget,QTableWidget,QTableWidgetItem,QApplication,QTableWidgetSelectionRange
+from PyQt4.QtCore import Qt, QString
+from PyQt4.QtTest import QTest
+import unittest
+import main
+import sys
+import random
+from view.MainWindow import MainWindow
+from view.Utils import initParent
+
+class mytest(unittest.TestCase): 
+    def setUp(self):
+        self.main = MainWindow()
+        self.inittablewidget = self.main.microcodeTableWidget
+        #self.inittablewidget.setSelectionMode(QAbstractItemView.ContiguousSelection)
+
+    def tearDown(self):
+        self.main = None
+        self.inittablewidget = None
+ 
+    def testdeleteLeftCellfirst(self):
+        self.inittablewidget.setItem(1,1,QTableWidgetItem("hello"))
+        self.inittablewidget.setItem(1,2,QTableWidgetItem("world"))
+        self.inittablewidget.setItem(1,3,QTableWidgetItem("china"))
+        selrange = QTableWidgetSelectionRange(1,1,1,2)
+        self.inittablewidget.setRangeSelected(selrange,True)
+        self.inittablewidget.currentRowNum = selrange.rowCount() 
+        self.inittablewidget.currentTopRow = selrange.topRow()
+        self.inittablewidget.currentColumnNum = selrange.columnCount() 
+        self.inittablewidget.currentLeftColumn = selrange.leftColumn()
+        self.inittablewidget.deleteCellLeft()
+        self.result = self.inittablewidget.item(1,1).text()
+        self.results = self.inittablewidget.item(1,2).text()
+        self.resultss = self.inittablewidget.item(1,3).text()
+        self.expectresult = QString("china")
+        self.expectresults = QString("")
+        self.assertEqual(self.result,self.expectresult)
+        self.assertEqual(self.results,self.expectresults)
+        self.assertEqual(self.resultss,self.expectresults)
+
+    def testdeleteLeftCellsecond(self):
+        row = random.randint(0,1999)
+        column = random.randint(0,19)
+        self.inittablewidget.setItem(row, column - 1, QTableWidgetItem("hello"))
+        self.inittablewidget.setItem(row, column, QTableWidgetItem("world"))
+        self.inittablewidget.setItem(row, column + 1, QTableWidgetItem("china"))
+        selrange = QTableWidgetSelectionRange(row, column - 1, row , column )
+        self.inittablewidget.setRangeSelected(selrange,True)
+        self.inittablewidget.currentRowNum = selrange.rowCount() 
+        self.inittablewidget.currentTopRow = selrange.topRow()
+        self.inittablewidget.currentColumnNum = selrange.columnCount() 
+        self.inittablewidget.currentLeftColumn = selrange.leftColumn()
+        self.inittablewidget.deleteCellLeft()
+        self.inittablewidget.setRangeSelected(selrange, False)
+        self.result = self.inittablewidget.item(row, column - 1).text()
+        self.results = self.inittablewidget.item(row, column).text()
+        self.resultss = self.inittablewidget.item(row, column + 1).text()
+        self.expectresult = QString("china")
+        self.expectresults = QString("")
+        self.assertEqual(self.result,self.expectresult)
+        self.assertEqual(self.results,self.expectresults)
+        self.assertEqual(self.resultss,self.expectresults)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(mytest)
+unittest.TextTestRunner(verbosity=2).run(suite)
