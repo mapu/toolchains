a7189b299b392730386771480f45d391d64f730f
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2016-04-26 05:50:49 +0000

    lij:add test file
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@621 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a
    
    Former-commit-id: 73ea0b0a3587debc28fc3a2ce47888cd7cbb63bf

--- asm-gui/test/testpaintRect.py
+++ asm-gui/test/testpaintRect.py
@@ -10,7 +10,6 @@ import sys
 import random
 from view.MainWindow import MainWindow
 from view.Utils import initParent
-from data.RectInfo import RectInfo
 
 class mytest(unittest.TestCase): 
     def setUp(self):
@@ -23,13 +22,80 @@ class mytest(unittest.TestCase):
         self.inittablewidget = None
 
     def testpaintRect(self):
-        selrange = QTableWidgetSelectionRange(0,0,0,0)
+        selrange = QTableWidgetSelectionRange(0,1,0,1)
         self.inittablewidget.setRangeSelected(selrange,True)
         self.main.register0Text.setText("3")
+        self.main.register1Text.setText("5")
+        self.main.register2Text.setText("7")
+        self.main.register3Text.setText("9")
         #self.inittablewidget.paintRect(self.main.register[0], self.main.register0Text.text())
         self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[0]), self.main.register0Text.text())
-        self.expectarray = "0,.0,.0,.0,."
-        self.assertEqual(self.inittablewidget.array[0][0], self.expectarray)
+        self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[1]), self.main.register1Text.text())
+        self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[2]), self.main.register1Text.text())
+        self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[3]), self.main.register1Text.text())
+        self.expectarray = "0,1,2,3,.0,1,2,3,.0,1,2,3,.0,1,2,3,."
+        self.assertEqual(self.inittablewidget.array[0][1], self.expectarray)
+
+    def testpaintRectfirst(self):
+        row = random.randint(0, 1999)
+        column = random.randint(0, 19)
+        varint = random.randint(0,99)
+        selrange = QTableWidgetSelectionRange(row, column, row, column)
+        self.inittablewidget.setRangeSelected(selrange,True)
+        self.main.register0Text.setText(str(varint))
+        self.main.register1Text.setText(str(varint))
+        self.main.register2Text.setText(str(varint))
+        self.main.register3Text.setText(str(varint))
+        #self.inittablewidget.paintRect(self.main.register[0], self.main.register0Text.text())
+        self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[0]), self.main.register0Text.text())
+        self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[1]), self.main.register1Text.text())
+        self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[2]), self.main.register1Text.text())
+        self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[3]), self.main.register1Text.text())
+        self.expectarray = "0,1,2,3,.0,1,2,3,.0,1,2,3,.0,1,2,3,."
+        self.assertEqual(self.inittablewidget.array[row][column], self.expectarray)
+
+    def testpaintRectsecoond(self):
+        row = random.randint(0, 1999)
+        column = random.randint(0, 19)
+        varint = random.randint(0,99)
+        varColumn = random.randint(1, 20 - column)
+        selrange = QTableWidgetSelectionRange(row, column, row, column + varColumn)
+        self.inittablewidget.setRangeSelected(selrange,True)
+        self.main.register0Text.setText(str(varint))
+        self.main.register1Text.setText(str(varint))
+        self.main.register2Text.setText(str(varint))
+        self.main.register3Text.setText(str(varint))
+        #self.inittablewidget.paintRect(self.main.register[0], self.main.register0Text.text())
+        re0 = self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[0]), self.main.register0Text.text())
+        re1 = self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[1]), self.main.register1Text.text())
+        re2 = self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[2]), self.main.register1Text.text())
+        re3 = self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[3]), self.main.register1Text.text())
+        self.expectresult = 0
+        self.assertEqual(re0, self.expectresult)
+        self.assertEqual(re1, self.expectresult)
+        self.assertEqual(re2, self.expectresult)
+        self.assertEqual(re3, self.expectresult)
+
+    def testpaintRectthird(self):
+        row = random.randint(0, 1999)
+        column = random.randint(0, 19)
+        varint = random.randint(0,99)
+        selrange = QTableWidgetSelectionRange(row, column, row, column )
+        self.inittablewidget.setRangeSelected(selrange,False)
+        self.main.register0Text.setText(str(varint))
+        self.main.register1Text.setText(str(varint))
+        self.main.register2Text.setText(str(varint))
+        self.main.register3Text.setText(str(varint))
+        #self.inittablewidget.paintRect(self.main.register[0], self.main.register0Text.text())
+        re0 = self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[0]), self.main.register0Text.text())
+        re1 = self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[1]), self.main.register1Text.text())
+        re2 = self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[2]), self.main.register1Text.text())
+        re3 = self.inittablewidget.paintRect(self.main.registerCheck.index(self.main.registerCheck[3]), self.main.register1Text.text())
+        self.expectresult = -1
+        self.assertEqual(re0, self.expectresult)
+        self.assertEqual(re1, self.expectresult)
+        self.assertEqual(re2, self.expectresult)
+        self.assertEqual(re3, self.expectresult)
 
 suite = unittest.TestLoader().loadTestsFromTestCase(mytest)
 unittest.TextTestRunner(verbosity=2).run(suite)
