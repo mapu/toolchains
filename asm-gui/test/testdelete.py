09e7bf092867a60b06872a5f26730f49b4080ae2
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2016-05-09 05:05:26 +0000

    lij:
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@685 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a
    
    Former-commit-id: 4a82a47844d56a6502889c529f7e20031a9f3da9

--- asm-gui/test/testdelete.py
+++ asm-gui/test/testdelete.py
@@ -5,8 +5,9 @@ from PyQt4.QtGui import QWidget,QTableWidget,QTableWidgetItem,QApplication,QTabl
 from PyQt4.QtCore import Qt, QString,QStringList
 from PyQt4.QtTest import QTest
 import unittest
-import main
 import sys
+sys.path.append("..")
+import main
 import random
 from view.MainWindow import MainWindow
 from view.Utils import initParent
@@ -15,13 +16,13 @@ class mytest(unittest.TestCase):
     def setUp(self):
         self.main = MainWindow()
         self.inittablewidget = self.main.microcodeTableWidget
-        self.inittablewidget.setSelectionMode(QAbstractItemView.ContiguousSelection)
 
     def tearDown(self):
         self.main = None
         self.inittablewidget = None
 
     def testDelete(self):
+        self.main.newFile()
         self.inittablewidget.setItem(0,0,QTableWidgetItem("hello"))
         selrange = QTableWidgetSelectionRange(0,0,0,0)
         self.inittablewidget.setRangeSelected(selrange,True)
@@ -30,34 +31,20 @@ class mytest(unittest.TestCase):
         self.item = QString("")
         self.assertTrue(self.inittablewidget.delete)
         self.assertEqual(stringss, self.item)
-  
-    def testdeletefirst(self):
-        self.inittablewidget.setItem(0,0,QTableWidgetItem("hello"))
-        self.inittablewidget.setItem(0,1,QTableWidgetItem("world"))
-        self.inittablewidget.setItem(0,2,QTableWidgetItem("china"))
-        selrange = QTableWidgetSelectionRange(0,0,0,2)
-        self.inittablewidget.setRangeSelected(selrange,True)
-        self.inittablewidget.delete()
-        string = self.inittablewidget.item(0,0).text()
-        strings = self.inittablewidget.item(0,1).text()
-        stringss = self.inittablewidget.item(0,2).text()
-        self.item = QString("")
-        self.assertEqual(string, self.item)
-        self.assertEqual(strings, self.item)
-        self.assertEqual(stringss, self.item) 
 
-    def testdeletesecond(self): 
+    def testDelete_1(self):
+        self.main.newFile() 
         row = random.randint(0,1999)
-        column = random.randint(0,19)      
-        self.inittablewidget.setItem(row, column, QTableWidgetItem("hello"))
-        self.inittablewidget.setItem(row, column + 1,QTableWidgetItem("world"))
-        self.inittablewidget.setItem(row, column + 2,QTableWidgetItem("china"))
-        selrange = QTableWidgetSelectionRange(row, column, row , column + 2)
+        column = 0      
+        self.inittablewidget.setItem(row, column, QTableWidgetItem("r0.m[0]->m[0]"))
+        self.inittablewidget.setItem(row + 1, column,QTableWidgetItem("r0.m[0]->m[1]"))
+        self.inittablewidget.setItem(row + 2, column,QTableWidgetItem("r0.m[0]->m[2]"))
+        selrange = QTableWidgetSelectionRange(row, column, row + 2, column)
         self.inittablewidget.setRangeSelected(selrange,True)
         self.inittablewidget.delete()
         string = self.inittablewidget.item(row, column).text()
-        strings = self.inittablewidget.item(row, column + 1).text()
-        stringss = self.inittablewidget.item(row, column + 2).text()
+        strings = self.inittablewidget.item(row + 1, column).text()
+        stringss = self.inittablewidget.item(row + 2, column).text()
         self.item = QString("")
         self.assertEqual(string, self.item)
         self.assertEqual(strings, self.item)
