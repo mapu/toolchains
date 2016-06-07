b9ed0a048be79fc6f2cfa6bc352a9a599b8a6fd5
litt <litt@cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a>
2016-06-07 05:24:31 +0000

    lij:
    git-svn-id: file:///opt/svnrepo/mapu/toolchains/trunk@768 cb94f8c2-beb9-42d2-aaaf-6dc30ea5c36a
    
    Former-commit-id: eed50b90cefab860e3752f9581cf854703c04289

--- asm-gui/test/TestMCCTreeWidget.py
+++ asm-gui/test/TestMCCTreeWidget.py
@@ -25,12 +25,121 @@ class mytest(unittest.TestCase):
 
     def testMCCTreeWidget(self):
         self.main.newFile()
-        selranges = QTableWidgetSelectionRange(0, 0, 0, 0)
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row, column, row, column)
         self.inittablewidget.setRangeSelected(selranges, True)
-        self.result = QString("r0")
-        self.mcctreewidget.searchMcc(0,0,self.result)
-        print self.result, self.mcctreewidget.result
+        self.result = QString("r")
+        self.mcctreewidget.searchMcc(row,column,self.result)
+        print self.mcctreewidget.result
 
+    def testMCCTreeWidget_0(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row, column, row, column)
+        self.inittablewidget.setRangeSelected(selranges, True)
+        self.mcctreewidget.searchMcc(row,column,"t")
+        print self.mcctreewidget.result
+  
+    def testMCCTreeWidget_1(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges, True)
+        self.mcctreewidget.searchMcc(row,column,"s")
+        print self.mcctreewidget.result
 
+    def testMCCTreeWidget_2(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges, True)
+        self.mcctreewidget.searchMcc(row,column,"b")
+        print self.mcctreewidget.result
+
+    def testMCCTreeWidget_3(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges, True)
+        self.mcctreewidget.searchMcc(row,column,"m")
+        print self.mcctreewidget.result
+
+    def testMCCTreeWidget_4(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges,True)
+        self.mcctreewidget.searchMcc(row,column,"a")
+        print self.mcctreewidget.result
+
+    def testMCCTreeWidget_5(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges,True)
+        self.mcctreewidget.searchMcc(row,column,"n")
+        print self.mcctreewidget.result
+
+    def testMCCTreeWidget_6(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges,True)
+        self.mcctreewidget.searchMcc(row,column,"e")
+        print self.mcctreewidget.result
+        
+    def testMCCTreeWidget_7(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges, True)
+        self.mcctreewidget.searchMcc(row,column,"c")
+        print self.mcctreewidget.result
+    
+    def testMCCTreeWidget_8(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges, True)
+        self.mcctreewidget.searchMcc(row,column,"d")
+        print self.mcctreewidget.result 
+     
+    def testMCCTreeWidget_9(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges, True)
+        self.mcctreewidget.searchMcc(row,column,"i")
+        print self.mcctreewidget.result
+        
+    def testMCCTreeWidget_10(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges, True)
+        self.mcctreewidget.searchMcc(row,column,"u")
+        print self.mcctreewidget.result
+        
+    def testMCCTreeWidget_11(self):
+        self.main.newFile()
+        column = 0
+        row = random.randint(0,2000)
+        selranges = QTableWidgetSelectionRange(row,column,row,column)
+        self.inittablewidget.setRangeSelected(selranges, True)
+        self.mcctreewidget.searchMcc(row,column,"l")
+        print self.mcctreewidget.result     
+        
 suite = unittest.TestLoader().loadTestsFromTestCase(mytest)
 unittest.TextTestRunner(verbosity=2).run(suite)
